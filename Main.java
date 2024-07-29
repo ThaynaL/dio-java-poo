1f2ec6e63abff7808ad95443b628e2e217f443bd
Thayna Lima <107405071+ThaynaL@users.noreply.github.com>
2024-07-29 18:03:51 -0300

    Add Main

---
+++ Main.java
@@ -0,0 +1,60 @@
+package br;
+import br.com.dio.desafio.dominio.Bootcamp;
+import br.com.dio.desafio.dominio.Curso;
+import br.com.dio.desafio.dominio.Dev;
+import br.com.dio.desafio.dominio.Mentoria;
+
+import java.time.LocalDate;
+
+public class Main {
+    public static void main(String[] args) {
+        Curso cursoJava = new Curso();
+        cursoJava.setTitulo("curso java");
+        cursoJava.setDescricao("descrição curso java");
+        cursoJava.setCargaHoraria(8);
+
+        Curso cursoJS = new Curso();
+        cursoJS.setTitulo("curso js");
+        cursoJS.setDescricao("descrição de um cursi de java script");
+        cursoJS.setCargaHoraria(4);
+
+        Mentoria mentoria = new Mentoria();
+        mentoria.setTitulo("mentoria de Node");
+        mentoria.setDescricao("descrição mentoria Node");
+        mentoria.setData(LocalDate.now());
+
+        Bootcamp bootcamp = new Bootcamp();
+        bootcamp.setNome("Bootcamp Java Developer");
+        bootcamp.setDescricao("Descrição Bootcamp Java Developer");
+        bootcamp.getConteudos().add(cursoJava);
+        bootcamp.getConteudos().add(cursoJS);
+        bootcamp.getConteudos().add(mentoria);
+
+        Dev devAline = new Dev();
+        devAline.setNome("Aline");
+        devAline.inscreverBootcamp(bootcamp);
+        System.out.println("Conteúdos Inscritos Aline:" + devAline.getConteudosInscritos());
+        devAline.progredir();
+        devAline.progredir();
+        System.out.println("-");
+        System.out.println("Conteúdos Inscritos Aline:" + devAline.getConteudosInscritos());
+        System.out.println("Conteúdos Concluídos Aline:" + devAline.getConteudosConcluidos());
+        System.out.println("XP:" + devAline.calcularTotalXP());
+
+        System.out.println("-------");
+
+        Dev devLucas = new Dev();
+        devLucas.setNome("Lucas");
+        devLucas.inscreverBootcamp(bootcamp);
+        System.out.println("Conteúdos Inscritos Lucas:" + devLucas.getConteudosInscritos());
+        devLucas.progredir();
+        devLucas.progredir();
+        devLucas.progredir();
+        System.out.println("-");
+        System.out.println("Conteúdos Inscritos Lucas:" + devLucas.getConteudosInscritos());
+        System.out.println("Conteúdos Concluidos Lucas:" + devLucas.getConteudosConcluidos());
+        System.out.println("XP:" + devLucas.calcularTotalXP());
+
+    }
+
+}
